106f28ea520ba74914576a7566972243d04f772f
tanyaflos76 <flos07@yandex.ru>
2024-11-10 20:35:01 +0300

    adding dialog form

---
+++ dialog.py
@@ -0,0 +1,23 @@
+import sys
+
+from PyQt6 import uic, QtCore, QtWidgets
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class MyDialog(QtWidgets.QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('dialog_form.ui', self)
+
+
+if __name__ == '__main__':
+    if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+
+    if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+    app = QApplication(sys.argv)
+    ex = Navigation()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())

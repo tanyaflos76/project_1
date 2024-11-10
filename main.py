2d5054961e92e75fd1cb66877b1347e16faa2a8e
tanyaflos76 <flos07@yandex.ru>
2024-11-10 19:43:31 +0300

    button "showClass'

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ class Navigation(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('template.ui', self)
-        self.ClassNumber.setVisible(False)
+        self.classNumber.setVisible(False)
         self.label_2.setVisible(False)
         self.showClass.setVisible(False)
         self.findClass.clicked.connect(self.find)
+        self.showClass.clicked.connect(self.show_number)
 
         self.pixmap = QPixmap('school.jpg')
         self.image = self.for_picture
@@ -21,9 +22,12 @@ class Navigation(QMainWindow):
         self.image.setPixmap(self.pixmap)
 
     def find(self):
-        self.ClassNumber.setVisible(True)
+        self.classNumber.setVisible(True)
         self.label_2.setVisible(True)
         self.showClass.setVisible(True)
+
+    def show_number(self):
+        number = int(self.classNumber.text())
 
 
 if __name__ == '__main__':

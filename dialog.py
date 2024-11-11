e4a210a0f3733889a2f6d6fbe55d34c0163626ba
tanyaflos76 <flos07@yandex.ru>
2024-11-11 22:57:21 +0300

    Show plans of the floor

--- dialog.py
+++ dialog.py
@@ -2,6 +2,7 @@ import sys
 
 from PyQt6 import uic, QtCore, QtWidgets
 from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtGui import QPixmap
 
 
 class MyDialog(QtWidgets.QDialog):
@@ -9,8 +10,19 @@ class MyDialog(QtWidgets.QDialog):
         super().__init__()
         uic.loadUi('dialog_form.ui', self)
 
-    def getVarToDialog(self):
-        self.label.setText('fvnejvjerbn')
+    def getVarToDialog(self, filename, number_floor):
+        # Отображаем в label номер этажа
+        self.nameOfTheFloor.setText(f'{number_floor} этаж')
+        # Отображаем саму картинку
+        self.pixmap = QPixmap(filename)
+        self.image = self.label
+        if filename == 'floors_picture/1_floor' or 'floors_picture/2_floor':
+            self.image.move(162, 40)
+            self.image.resize(1060, 670)
+        else:
+            self.image.move(0, 0)
+            self.image.resize(1000, 670)
+        self.image.setPixmap(self.pixmap)
 
 
 if __name__ == '__main__':

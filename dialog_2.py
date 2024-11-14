dd33f15804eecf66b1b60cee92d6cbc67fdc203a
tanyaflos76 <flos07@yandex.ru>
2024-11-14 21:37:10 +0300

    show information

--- dialog_2.py
+++ dialog_2.py
@@ -9,9 +9,29 @@ class MyDialog_2(QtWidgets.QDialog):
     def __init__(self):
         super().__init__()
         uic.loadUi('dialog_form_2.ui', self)
+        self.numberClass.setEnabled(False)
+        self.teacher.setEnabled(False)
+        self.floor.setEnabled(False)
+        self.subject.setEnabled(False)
+        self.teacher_2.setVisible(False)
+        self.subject_2.setVisible(False)
+        self.label_4.setVisible(False)
+        self.label_8.setVisible(False)
 
-    def getVarToDialog_2(self):
-        pass
+    def getVarToDialog_2(self, number, number_floor, teacher, subject):
+        self.numberClass.setText(number)
+        self.floor.setText(number_floor)
+        if len(teacher) > 1:
+            self.teacher_2.setVisible(True)
+            self.subject_2.setVisible(True)
+            self.label_4.setVisible(True)
+            self.label_8.setVisible(True)
+            self.subject_2.setEnabled(False)
+            self.teacher_2.setEnabled(False)
+            self.teacher_2.setText(teacher[1])
+            self.subject_2.setText(subject[1])
+        self.teacher.setText(teacher[0])
+        self.subject.setText(subject[0])
 
 
 if __name__ == '__main__':
